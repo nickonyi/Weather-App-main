4a94f074fe98c2a9572e74253112404bf0c67b12
nickonyi <nickoluoch1130@gmail.com>
2025-09-07 21:31:31 +0300

    installed sass and added sass variables at the app main sass file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./components/navbar";
 import SearchBox from "./components/searchBox";
 import MainContent from "./components/mainContent";
-import "./styles/App.css";
+import "./styles/App.scss";
 
 function App() {
   return (
