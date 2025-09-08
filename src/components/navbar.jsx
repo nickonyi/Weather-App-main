2099fdd1344b14ba9ebaeaf5030b284df2c0ac7d
nickonyi <nickoluoch1130@gmail.com>
2025-09-08 23:19:42 +0300

    feat: implement Navbar component with styling adjustments

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import logo from "../assets/images/logo.svg";
+import iconUnit from "../assets/images/icon-units.svg";
+import iconDropdown from "../assets/images/icon-dropdown.svg";
 
 function Navbar() {
-  return <div>navbar</div>;
+  return (
+    <nav className="nav-container flex justify-between items-center">
+      <div className="nav-logo">
+        <div className="nav-logo-img">
+          <img src={logo} alt="Weather App Logo" />
+        </div>
+      </div>
+
+      <div className="metric-container flex items-center gap-2 cursor-pointer rounded">
+        <img src={iconUnit} alt="Celsius Icon" />
+        <button className="metric-button">Units</button>
+        <img src={iconDropdown} alt="Dropdown Icon" />
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
