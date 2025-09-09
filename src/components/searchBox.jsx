0c4967890727ef572bb22ef0dbf7bb85f40cd61d
nickonyi <nickoluoch1130@gmail.com>
2025-09-09 22:27:21 +0300

    feat: update SearchBox component with improved styling and layout

--- src/components/searchBox.jsx
+++ src/components/searchBox.jsx
@@ -6,13 +6,19 @@ function SearchBox() {
       <p className="searchbox-text w-fit text-5xl">
         How's the sky looking today?
       </p>
-      <div className="search-box-input-container w-fit">
+      <div className="search-box-input-container w-fit flex gap-4">
         <input
           type="text"
-          className="search-input"
+          id="search-input"
+          className="border rounded-md px-24 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           placeholder="Search for a place..."
         />
-        <button id="search-btn">Search</button>
+        <button
+          id="search-btn"
+          className="border rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        >
+          Search
+        </button>
       </div>
     </div>
   );
