2f83c11423be346974f6fbab4a5a72d98fd5311f
nickonyi <nickoluoch1130@gmail.com>
2025-09-18 15:22:15 +0300

    feat: update dropdown styling and improve date selection options

--- src/components/mainContent.jsx
+++ src/components/mainContent.jsx
@@ -125,15 +125,15 @@ function MainContent() {
               <select
                 name="date"
                 id="date-select"
-                className="border p-2 bg-gray-500"
+                className="border w-32 rounded-lg  p-2 bg-gray-500"
               >
+                <option value="monday">Monday</option>
                 <option value="tuesday">Tuesday</option>
                 <option value="wednesday">Wednesday</option>
                 <option value="thursday">Thursday</option>
                 <option value="friday">Friday</option>
                 <option value="saturday">Saturday</option>
                 <option value="sunday">Sunday</option>
-                <option value="monday">Monday</option>
               </select>
             </div>
           </div>
