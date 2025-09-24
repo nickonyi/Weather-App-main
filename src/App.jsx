c99cf5fbc6c770c0d074b55784ea7d0347bd07af
nickonyi <nickoluoch1130@gmail.com>
2025-09-24 18:03:51 +0300

    feat: enhance styling for weather date and metrics in MainContent and App.scss

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Error from "./pages/Error";
 import { ErrorBoundary } from "react-error-boundary";
 import "./styles/App.scss";
 import "./styles/index.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [weather, setWeather] = useState(null);
@@ -15,6 +15,54 @@ function App() {
     wind: "kmh",
     precipitation: "mm",
   });
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (pos) => {
+        setLoading(true);
+        try {
+          const latitude = pos.coords.latitude;
+          const longitude = pos.coords.longitude;
+
+          const urlo = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+          const resp = await fetch(urlo);
+          const location = await resp.json();
+          const city = location.city;
+          const country = location.country;
+
+          const params = new URLSearchParams({
+            latitude,
+            longitude,
+            current:
+              "temperature_2m,relative_humidity_2m,precipitation,weathercode,windspeed_10m",
+            hourly: "temperature_2m,weathercode",
+            daily:
+              "temperature_2m_max,temperature_2m_min,weathercode,precipitation_sum",
+            timezone: "auto",
+            temperature_unit: units.temperature,
+            windspeed_unit: units.wind,
+            precipitation_unit: units.precipitation,
+          });
+
+          const url = `https://api.open-meteo.com/v1/forecast?${params.toString()}`;
+          const res = await fetch(url);
+          const data = await res.json();
+
+          setWeather({
+            city,
+            country,
+            current: data.current,
+            daily: data.daily,
+            hourly: data.hourly,
+          });
+        } catch (error) {
+          console.error("Geolocation fetch error:", error);
+        } finally {
+          setLoading(false);
+        }
+      });
+    }
+  }, []);
 
   return (
     <>
