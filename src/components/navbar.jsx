1ef5d7f039bef178396f5b2cfcde277c74e576e6
nickonyi <nickoluoch1130@gmail.com>
2025-10-05 20:08:15 +0300

    feat: enhance Navbar component to include favorites functionality and improve UI interactions

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -65,7 +65,7 @@ function Navbar({ units, setUnits, favorites, onSelectFavorite }) {
                 </p>
               ))
             ) : (
-              <p className="text-gray-500">No favorite yet!</p>
+              <p className="text-white-500">No favorite yet!</p>
             )}
           </div>
         </div>
