c67db0e5ca0a5b3eeab145e0a4cb919857aa2512
nickonyi <nickoluoch1130@gmail.com>
2025-10-08 15:28:23 +0300

    chore: remove unused README and temperature options files

--- txt.js
+++
@@ -1,8 +0,0 @@
-const temperatureOptions = {
-  celsius: "Celsius (°C)",
-  fahrenheit: "Fahrenheit (°F)",
-};
-
-console.log(
-  Object.entries(temperatureOptions).map(([label, value]) => [label, value])
-);
